186a28ba985b1ddca7cee2f9942838bdef3f458b
Jon Osmond <jonathan.osmond@gmail.com>
2025-07-05 17:07:53 -0500

    Keep filter panel open for multi-select

--- script.js
+++ script.js
@@ -2477,7 +2477,7 @@ async function init(){
       await loadPlants();
       checkArchivedLink();
       updateFilterChips();
-      if (filterPanel) {
+      if (filterPanel && !roomFilter.hasAttribute('multiple')) {
         filterPanel.classList.remove('show');
         filterToggle.setAttribute('aria-expanded', 'false');
         filterPanel.setAttribute('aria-hidden', 'true');
